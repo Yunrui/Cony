aa8fa69ac74f3bf4c4fa964f5321c46a9d03e57c
YunRui SiMa <mylpis@hotmail.com>
2014-01-07 16:13:53 +0800

    Submit project template

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Cony
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Invoked when an item within the list is selected.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is Snapped)
+        /// displaying the selected item.</param>
+        /// <param name="e">Event data that describes how the selection was changed.</param>
+        void ItemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Invalidate the view state when logical page navigation is in effect, as a change
+            // in selection may cause a corresponding change in the current logical page.  When
+            // an item is selected this has the effect of changing from displaying the item list
+            // to showing the selected item's details.  When the selection is cleared this has the
+            // opposite effect.
+        }
+    }
+}
